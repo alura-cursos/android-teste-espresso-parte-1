a1d7df2a2b9b5e7658892a2b6bb033a6d5862018
Alex Felipe <alexfelipevieira@gmail.com>
2018-10-03 10:59:22 -0300

    vídeo 6.3

--- app/src/androidTest/java/br/com/alura/leilao/matchers/ViewMatcher.java
+++ app/src/androidTest/java/br/com/alura/leilao/matchers/ViewMatcher.java
@@ -11,12 +11,15 @@ import org.hamcrest.Matcher;
 import br.com.alura.leilao.R;
 import br.com.alura.leilao.formatter.FormatadorDeMoeda;
 
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+
 public class ViewMatcher {
 
     public static Matcher<? super View> apareceLeilaoNaPosicao(final int posicaoEsperada,
                                                                final String descricaoEsperada,
                                                                final double maiorLanceEsperado) {
         return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            private Matcher<View> displayed = isDisplayed();
             private final FormatadorDeMoeda formatador = new FormatadorDeMoeda();
             private final String maiorLanceFormatadoEsperado = formatador.formata(maiorLanceEsperado);
 
@@ -24,7 +27,9 @@ public class ViewMatcher {
             public void describeTo(Description description) {
                 description.appendText("View com descrição ").appendValue(descricaoEsperada)
                         .appendText(", maior lance ").appendValue(maiorLanceFormatadoEsperado)
-                        .appendText(" na posição ").appendValue(posicaoEsperada);
+                        .appendText(" na posição ").appendValue(posicaoEsperada)
+                .appendText(" ");
+                description.appendDescriptionOf(displayed);
             }
 
             @Override
@@ -37,7 +42,10 @@ public class ViewMatcher {
                 View viewDoViewHolder = viewHolderDevolvido.itemView;
                 boolean temDescricaoEsperada = verificaDescricaoEsperada(viewDoViewHolder);
                 boolean temMaiorLanceEsperado = verificaMaiorLanceEsperado(viewDoViewHolder);
-                return temDescricaoEsperada && temMaiorLanceEsperado;
+                displayed = isDisplayed();
+                return temDescricaoEsperada &&
+                        temMaiorLanceEsperado &&
+                        displayed.matches(viewDoViewHolder);
             }
 
             private boolean verificaMaiorLanceEsperado(View viewDoViewHolder) {
--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -13,16 +13,12 @@ import java.io.IOException;
 
 import br.com.alura.leilao.R;
 import br.com.alura.leilao.api.retrofit.client.TesteWebClient;
-import br.com.alura.leilao.formatter.FormatadorDeMoeda;
 import br.com.alura.leilao.model.Leilao;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static br.com.alura.leilao.matchers.ViewMatcher.apareceLeilaoNaPosicao;
-import static org.hamcrest.core.AllOf.allOf;
 
 public class ListaLeilaoTelaTest {
 
@@ -32,7 +28,6 @@ public class ListaLeilaoTelaTest {
     public ActivityTestRule<ListaLeilaoActivity> activity =
             new ActivityTestRule<>(ListaLeilaoActivity.class, true, false);
     private final TesteWebClient webClient = new TesteWebClient();
-    private final FormatadorDeMoeda formatadorDeMoeda = new FormatadorDeMoeda();
 
     @Before
     public void setup() throws IOException {
@@ -45,15 +40,8 @@ public class ListaLeilaoTelaTest {
 
         activity.launchActivity(new Intent());
 
-        onView(allOf(withText("Carro"),
-                withId(R.id.item_leilao_descricao)))
-                .check(matches(isDisplayed()));
-
-        String formatoEsperado = formatadorDeMoeda.formata(0.00);
-
-        onView(allOf(withText(formatoEsperado),
-                withId(R.id.item_leilao_maior_lance)))
-                .check(matches(isDisplayed()));
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .check(matches(apareceLeilaoNaPosicao(0, "Carro", 0.00)));
     }
 
     @Test
@@ -64,26 +52,11 @@ public class ListaLeilaoTelaTest {
 
         activity.launchActivity(new Intent());
 
-//        onView(allOf(withText("Carro"), withId(R.id.item_leilao_descricao)))
-//                .check(matches(isDisplayed()));
-//
-//        String formatoEsperadoParaCarro = formatadorDeMoeda.formata(0.00);
-//
-//        onView(allOf(withText(formatoEsperadoParaCarro),
-//                withId(R.id.item_leilao_maior_lance)))
-//                .check(matches(isDisplayed()));
-//
-//        onView(allOf(withText("Computador"), withId(R.id.item_leilao_descricao)))
-//                .check(matches(isDisplayed()));
-//
-//        String formatoEsperadoParaComputador = formatadorDeMoeda.formata(0.00);
-//
-//        onView(allOf(withText(formatoEsperadoParaComputador),
-//                withId(R.id.item_leilao_maior_lance)))
-//                .check(matches(isDisplayed()));
-
         onView(withId(R.id.lista_leilao_recyclerview))
                 .check(matches(apareceLeilaoNaPosicao(0, "Carro", 0.00)));
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .check(matches(apareceLeilaoNaPosicao(1, "Computador", 0.00)));
     }
 
     @After
